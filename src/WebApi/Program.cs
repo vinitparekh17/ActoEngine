3466f239931633dff3dfb106d986e6db69e20518
vinitparekh <vinitparekh1742@gmail.com>
2025-08-09 20:22:29 +0530

    cleanup

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -1,6 +1,3 @@
-using System.Text;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
 using MyApp.WebApi.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
